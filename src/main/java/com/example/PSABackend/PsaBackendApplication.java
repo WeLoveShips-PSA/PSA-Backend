9dad697120008ee8e747d810578dd5d6a152001d
ruixiantay <tayrx9988@gmail.com>
2021-02-01 21:36:06 +0800

    First commit

---
+++ src/main/java/com/example/PSABackend/PsaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.PSABackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PSABackend/PsaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PSABackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
