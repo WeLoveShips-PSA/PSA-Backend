d61adc9cb284754f81f7736a9008f2e63d61450a
bingyuyap <bingyu.yap.21@gmail.com>
2021-03-05 23:03:54 +0800

    Add Vessel Class

--- src/main/java/com/example/PSABackend/classes/Vessel.java
+++ src/main/java/com/example/PSABackend/classes/Vessel.java
@@ -1,4 +1,16 @@
 package com.example.PSABackend.classes;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import javax.validation.constraints.NotBlank;
+
 public class Vessel {
+    @NotBlank
+    private String abbr;
+    @NotBlank
+    private String voy;
+    @NotBlank
+    private String bthg;
+
+    private String fullVsim;
+    private String invoyn;
 }
