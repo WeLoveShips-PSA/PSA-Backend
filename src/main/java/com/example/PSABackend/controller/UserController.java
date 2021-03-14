0e877aa74b611b580dc191786cecd5ffc34120ab
bingyuyap <bingyu.yap.21@gmail.com>
2021-03-14 15:23:50 +0800

    Merge with shaun

--- src/main/java/com/example/PSABackend/controller/UserController.java
+++ src/main/java/com/example/PSABackend/controller/UserController.java
@@ -4,6 +4,7 @@ import com.example.PSABackend.classes.*;
 import com.example.PSABackend.exceptions.InvalidEmailException;
 import com.example.PSABackend.exceptions.UserAlreadyExistAuthenticationException;
 import com.example.PSABackend.service.UserService;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
