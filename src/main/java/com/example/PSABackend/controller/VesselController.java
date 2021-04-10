c3442e72863cf339d9f8a6c2e932111ddf9193bb
bingyuyap <bingyu.yap.21@gmail.com>
2021-04-10 14:29:32 +0800

    Fix returned Vessel HttpStatus

--- src/main/java/com/example/PSABackend/controller/VesselController.java
+++ src/main/java/com/example/PSABackend/controller/VesselController.java
@@ -3,8 +3,11 @@ package com.example.PSABackend.controller;
 import com.example.PSABackend.DAO.VesselDAS;
 import com.example.PSABackend.classes.Vessel;
 import com.example.PSABackend.classes.VesselDetails;
+import com.example.PSABackend.exceptions.DataException;
 import com.example.PSABackend.exceptions.PSAException;
 import com.example.PSABackend.service.VesselService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.parameters.P;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,9 +25,12 @@ public class VesselController {
 
     @GetMapping
     @RequestMapping(path = "/getall")
-    public ArrayList<VesselDetails> getAllVessels () {
+    public ResponseEntity<?> getAllVessels () {
         try {
-            return VesselService.getAllVessels();
+            List<VesselDetails> vesselList = VesselService.getAllVessels();
+            return ResponseEntity.status(HttpStatus.OK).body(vesselList);
+        } catch (DataException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
         } catch (PSAException e) {
             return null;
         }
@@ -32,11 +38,14 @@ public class VesselController {
 
     @PostMapping
     @RequestMapping(path = "/getvessel")
-    public Vessel getVesselById (@RequestBody Map<String, String> body) {
+    public ResponseEntity<?> getVesselById (@RequestBody Map<String, String> body) {
         String abbrVslM = body.get("abbrVslM");
         String inVoyN = body.get("inVoyN");
         try {
-            return VesselService.getVesselById(abbrVslM, inVoyN);
+            Vessel vessel = VesselService.getVesselById(abbrVslM, inVoyN);
+            return ResponseEntity.status(HttpStatus.OK).body(vessel);
+        } catch (DataException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
         } catch (PSAException e) {
             return null;
         }
@@ -44,10 +53,13 @@ public class VesselController {
 
     @PostMapping
     @RequestMapping(path = "/get-vessel-by-shortAbbrVslM")
-    public List<VesselDetails> getVesselByAbbrVslM (@RequestBody Map<String, String> body) {
+    public ResponseEntity<?> getVesselByAbbrVslM (@RequestBody Map<String, String> body) {
         String shortAbbrVslM = body.get("abbrVslM");
         try {
-            return VesselService.getVesselByAbbrVslM(shortAbbrVslM);
+            List<VesselDetails> vesselList = VesselService.getVesselByAbbrVslM(shortAbbrVslM);
+            return ResponseEntity.status(HttpStatus.OK).body(vesselList);
+        } catch (DataException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
         } catch (PSAException e) {
             return null;
         }
@@ -55,13 +67,16 @@ public class VesselController {
 
     @PostMapping
     @RequestMapping(path = "/getvesselsbydate")
-    public List<VesselDetails> getVesselsByDate (@RequestBody Map<String, String> body) {
+    public ResponseEntity<?> getVesselsByDate (@RequestBody Map<String, String> body) {
         String date = body.get("date");
         //sort by
         //order
         LocalDateTime dateTime = LocalDateTime.parse(date);
         try {
-            return VesselService.getVesselsByDate(dateTime);
+            List<VesselDetails> vesselList = VesselService.getVesselsByDate(dateTime);
+            return ResponseEntity.status(HttpStatus.OK).body(vesselList);
+        } catch (DataException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
         } catch (PSAException e) {
             return null;
         }
@@ -69,11 +84,14 @@ public class VesselController {
 
     @PostMapping
     @RequestMapping(path= "/get-vessel-speed-history")
-    public List<TreeMap> getVesselSpeedHistory (@RequestBody Map<String, String> body) {
+    public ResponseEntity<?> getVesselSpeedHistory (@RequestBody Map<String, String> body) {
 
         String vsl_voy = body.get("vsl_voy").toString();
         try {
-            return VesselService.getVesselSpeedHistory(vsl_voy);
+            List<TreeMap> vesselSpeedHistory =  VesselService.getVesselSpeedHistory(vsl_voy);
+            return ResponseEntity.status(HttpStatus.OK).body(vesselSpeedHistory);
+        } catch (DataException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
         } catch (PSAException e) {
             return null;
         }
