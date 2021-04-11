60931e92789fb8e81193b3a98a05d595f53c8306
bingyuyap <bingyu.yap.21@gmail.com>
2021-04-11 17:24:00 +0800

    Delete unused Classes Add Hashed Password functionalities Hide email passwords

--- src/main/java/com/example/PSABackend/service/EmailService.java
+++ src/main/java/com/example/PSABackend/service/EmailService.java
@@ -1,30 +1,70 @@
 package com.example.PSABackend.service;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Repository;
+
 import java.io.IOException;
 import java.util.Date;
 import java.util.Properties;
 import javax.mail.*;
 import javax.mail.internet.*;
+import javax.validation.constraints.Email;
 
+@Repository
 public class EmailService {
+    private static String email;
+    private static String password;
+    private static String auth;
+    private static String starttlsEnable;
+    private static String host;
+    private static String port;
+
+    @Value("${portnet.email.from}")
+    public void setMail(String value) {
+        this.email = value;
+    }
+
+    @Value("${portnet.mail.password}")
+    public void setPassword(String value) {
+        this.password = value;
+    }
+
+    @Value("${portnet.mail.properties.mail.smtp.auth}")
+    public void setAuth(String value) {
+        this.auth = value;
+    }
+    @Value("${portnet.mail.properties.mail.smtp.starttls.enable}")
+    public void setStarttlsEnable(String value) {
+        this.starttlsEnable = value;
+    }
+
+    @Value("${portnet.mail.properties.mail.smtp.host}")
+    public void setHost(String value) {
+        this.host = value;
+    }
+
+    @Value("${portnet.mail.properties.mail.smtp.port}")
+    public void setdPort(String value) {
+        this.port = value;
+    }
 
     public static String sendEmail(String email, String body, String head,String userName) throws AddressException, MessagingException, IOException {
         Properties props = new Properties();
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.smtp.host", "smtp.gmail.com");
-        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.auth", EmailService.auth);
+        props.put("mail.smtp.starttls.enable", EmailService.starttlsEnable);
+        props.put("mail.smtp.host", EmailService.host);
+        props.put("mail.smtp.port", EmailService.port);
 
         //input username and password
         Session session = Session.getInstance(props, new javax.mail.Authenticator() {
             protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication("cs102psa@gmail.com", "cs102javapsadabdab");
+                return new PasswordAuthentication(EmailService.email, EmailService.password);
             }
         });
 
         Message msg = new MimeMessage(session);
         //sender email
-        msg.setFrom(new InternetAddress("cs102psa@gmail.com", false));
+        msg.setFrom(new InternetAddress(EmailService.email, false));
 
         //content should be dependent on alert class
         //to be changed
@@ -41,45 +81,6 @@ public class EmailService {
 //        MimeBodyPart attachPart = new MimeBodyPart();
 
         Transport.send(msg);
-        //success message
         return "Email sent successfully!";
     }
-
-
-//    public static String changeInPredicted_BTR_sendEmail(String recipient) throws AddressException, MessagingException, IOException {
-//        Properties props = new Properties();
-//        props.put("mail.smtp.auth", "true");
-//        props.put("mail.smtp.starttls.enable", "true");
-//        props.put("mail.smtp.host", "smtp.gmail.com");
-//        props.put("mail.smtp.port", "587");
-//
-//        //input username and password
-//        Session session = Session.getInstance(props, new javax.mail.Authenticator() {
-//            protected PasswordAuthentication getPasswordAuthentication() {
-//                return new PasswordAuthentication("cs102psa@gmail.com", "cs102javapsadabdab");
-//            }
-//        });
-//
-//        Message msg = new MimeMessage(session);
-//        //sender email
-//        msg.setFrom(new InternetAddress("cs102psa@gmail.com", false));
-//
-//        //content should be dependent on alert class
-//        //to be changed
-//        msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(recipient));
-//        msg.setSubject("Changes in predicted berthing time");
-//        msg.setContent("Good pm sir/mdm", "text/html");
-//        msg.setSentDate(new Date());
-//
-//        MimeBodyPart messageBodyPart = new MimeBodyPart();
-//        messageBodyPart.setContent("CS102 Java PSA Dab Dab", "text/html");
-//
-////        Multipart multipart = new MimeMultipart();
-////        multipart.addBodyPart(messageBodyPart);
-////        MimeBodyPart attachPart = new MimeBodyPart();
-//
-//        Transport.send(msg);
-//        //success message
-//        return "Email sent successfully!";
-//    }
 }
